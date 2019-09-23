9ad074e76e0510aff93ee8e7f0e7c402651c6c85
Suthon <suthon@gmail.com>
2019-09-23 13:38:02 +0700

    add command

--- load-test/simple.js
+++ load-test/simple.js
@@ -1,9 +1,10 @@
 import { check } from "k6";
-  import http from "k6/http";
-  
-  export default function() {
-      let res = http.get("http://localhost:3000");
-      check(res, {
-          "is status 200": (r) => r.status === 200
-      });
-  };
+import http from "k6/http";
+//k6 run --vus 10 --duration 30s load-test/simple.js
+
+export default function () {
+    let res = http.get("http://localhost:3000");
+    check(res, {
+        "is status 200": (r) => r.status === 200
+    });
+};
